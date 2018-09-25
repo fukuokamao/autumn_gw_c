07bea058d5155e517d05ffe713eb4d714200fce2
FukuokaMao <creamymao37@gmail.com>
2018-09-25 23:53:46 +0900

    音入れてみた

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager>{
+
+	[SerializeField]
+	private AudioClip[] Sound = new AudioClip[10];
+	private AudioSource audioSource;
+
+	override protected void Awake()
+    {
+        base.Awake();
+    }
+	
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)){
+			PlaySound(1);
+		}
+
+		if(Input.GetKeyDown(KeyCode.C)){
+			PlaySound(2);
+		}
+
+		if(Input.GetKeyDown(KeyCode.H)){
+			PlaySound(3);
+		}
+
+		if(Input.GetKeyDown(KeyCode.M)){
+			PlaySound(4);
+		}
+
+		if(Input.GetKeyDown(KeyCode.N)){
+			PlaySound(5);
+		}
+
+		if(Input.GetKeyDown(KeyCode.O)){
+			PlaySound(6);
+		}
+
+		if(Input.GetKeyDown(KeyCode.S)){
+			PlaySound(7);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Y)){
+			PlaySound(8);
+		}
+
+		if(Input.GetKeyDown(KeyCode.P)){
+			PlaySound(9);
+		}
+
+		if(Input.GetKeyDown(KeyCode.U)){
+			PlaySound(10);
+		}
+	}
+
+	public void PlaySound(int soundId){
+		switch(soundId){
+			case 1:
+			audioSource.clip = Sound[0];
+        	audioSource.Play ();
+			break;
+			case 2:
+			audioSource.clip = Sound[1];
+        	audioSource.Play ();
+			break;
+			case 3:
+			audioSource.clip = Sound[2];
+        	audioSource.Play ();
+			break;
+			case 4:
+			audioSource.clip = Sound[3];
+        	audioSource.Play ();
+			break;
+			case 5:
+			audioSource.clip = Sound[4];
+        	audioSource.Play ();
+			break;
+			case 6:
+			audioSource.clip = Sound[5];
+        	audioSource.Play ();
+			break;
+			case 7:
+			audioSource.clip = Sound[6];
+        	audioSource.Play ();
+			break;
+			case 8:
+			audioSource.clip = Sound[7];
+        	audioSource.Play ();
+			break;
+			case 9:
+			audioSource.clip = Sound[8];
+        	audioSource.Play ();
+			break;
+			case 10:
+			audioSource.clip = Sound[9];
+        	audioSource.Play ();
+			break;
+			default:
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/ThrowingObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingObject : MonoBehaviour {
+
+    public enum Food{
+        grape,
+        cupcake,  
+        chestnut
+    }
+
+
+    /// 投げるオブジェクト
+    [SerializeField, Tooltip("投げるオブジェクトをここに割り当てる")]
+    private GameObject throwingObject;
+
+    /// スタート位置
+    [SerializeField, Tooltip("発射始点をここに割り当てる")]
+    private GameObject startPos;
+	
+    /// ターゲット位置
+    [SerializeField, Tooltip("発射終点をここに割り当てる")]
+    private GameObject targetPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+            throwingFood(Food.cupcake);
+        }
+	}
+
+    // 食べ物を投げるメソッド
+    public void throwingFood(Food food){
+        switch (food)
+            {
+                case Food.grape:
+                Debug.Log("ぶどうを投げます");
+                    return;
+                case Food.cupcake:
+                Debug.Log("カップケーキを投げます");
+                    break;
+                case Food.chestnut:
+                Debug.Log("栗を投げます");
+                    break;
+                default:
+                    return;
+            }
+    }
+}
